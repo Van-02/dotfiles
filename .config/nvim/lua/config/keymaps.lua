4a39e16f7ec065675d99f96d0d9995a2d67bce66
Van-02 <lalikivan02@gmail.com>
2026-01-10 08:19:44 -0300

    add some keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+----- OIL -----
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+-- Delete all buffers but the current one
+vim.keymap.set(
+	"n",
+	"<leader>bq",
+	'<Esc>:%bdelete|edit #|normal`"<Return>',
+	{ desc = "Delete other buffers but the current one" }
+)
