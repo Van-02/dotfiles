e4f1c9b88280a505123eeedb1e4ba675bba81137
Van-02 <lalikivan02@gmail.com>
2026-01-10 06:30:50 -0300

    cleaning nvim

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -1,77 +1,43 @@
--- This file contains the configuration for setting up the lazy.nvim plugin manager in Neovim.
-
--- Node.js configuration - always use latest stable version
-vim.g.node_host_prog = vim.fn.exepath("node") or "/usr/local/bin/node"
-
--- Spell-checking
-vim.opt.spell = true -- activa spell checker
-vim.opt.spelllang = { "en" }
-
--- Define the path to the lazy.nvim plugin
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-
--- Check if the lazy.nvim plugin is not already installed
-if not vim.loop.fs_stat(lazypath) then
-    -- Bootstrap lazy.nvim by cloning the repository
-    -- stylua: ignore
-    vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable",
-        lazypath })
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
+vim.opt.rtp:prepend(lazypath)
 
--- Prepend the lazy.nvim path to the runtime path
-vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
-
--- Setup lazy.nvim with the specified configuration
 require("lazy").setup({
   spec = {
-    -- Add LazyVim and import its plugins
+    -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    -- Import any extra modules here
-    -- Editor plugins
-    { import = "lazyvim.plugins.extras.editor.harpoon2" },
-    { import = "lazyvim.plugins.extras.editor.mini-files" },
-    -- { import = "lazyvim.plugins.extras.editor.snacks_explorer" },
-    { import = "lazyvim.plugins.extras.editor.snacks_picker" },
-
-    -- Debgugging plugins
-    { import = "lazyvim.plugins.extras.dap.core" },
-
-    -- Formatting plugins
-    { import = "lazyvim.plugins.extras.formatting.biome" },
-    { import = "lazyvim.plugins.extras.formatting.prettier" },
-
-    -- Linting plugins
-    { import = "lazyvim.plugins.extras.linting.eslint" },
-
-    -- Language support plugins
-    { import = "lazyvim.plugins.extras.lang.json" },
-    { import = "lazyvim.plugins.extras.lang.markdown" },
-
-    -- Coding plugins
-    { import = "lazyvim.plugins.extras.coding.mini-surround" },
-    { import = "lazyvim.plugins.extras.editor.mini-diff" },
-    { import = "lazyvim.plugins.extras.coding.blink" },
-
-    -- Utility plugins
-    { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
-
-    -- Import/override with your plugins
+    -- import/override with your plugins
     { import = "plugins" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
     -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
     lazy = false,
-    -- It's recommended to leave version=false for now, since a lot of the plugins that support versioning
+    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
     -- have outdated releases, which may break your Neovim install.
-    version = false, -- Always use the latest git commit
-    -- version = "*", -- Try installing the latest stable version for plugins that support semver
+    version = false, -- always use the latest git commit
+    -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "tokyonight", "habamax" } }, -- Specify colorschemes to install
-  checker = { enabled = true }, -- Automatically check for plugin updates
+  install = { colorscheme = { "tokyonight", "habamax" } },
+  checker = {
+    enabled = true, -- check for plugin updates periodically
+    notify = false, -- notify on update
+  }, -- automatically check for plugin updates
   performance = {
     rtp = {
-      -- Disable some runtime path plugins to improve performance
+      -- disable some rtp plugins
       disabled_plugins = {
         "gzip",
         -- "matchit",
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,33 +1,3 @@
-local opt = vim.opt
-
--- Line numbers
-opt.number = true   	
-opt.relativenumber = true
-
--- Tabs
-opt.tabstop = 4		    	-- Un tabulador equivale a 4 espacios
-opt.softtabstop = 4
-opt.shiftwidth = 4	    	-- Tamaño de la sangrua automatica
-opt.expandtab = true	    	-- Convertir tabs en espacios
-
--- Indentation
-opt.smartindent = true
-
--- Don't wrap text
-opt.wrap = false
-
--- Searching
-opt.hlsearch = false
-opt.incsearch = true
-opt.ignorecase = true       	-- Ignorar mayúsculas al buscar
-opt.smartcase = true        	-- Si buscas con una mayúscula, se vuelve sensible
-
--- Colors
-opt.termguicolors = true    	-- Colores reales (necesario para temas modernos)
-
--- Vertical markers
-opt.signcolumn = 'yes'
-opt.colorcolumn = '80'
-
-opt.mouse = 'a'             	-- Habilitar ratón
-opt.clipboard = 'unnamedplus'	-- Usa el portapapeles del sistema 
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
